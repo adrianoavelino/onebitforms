e40e208769dff9e96358ad06f3aa3a6f82d31589
Adriano Avelino <adriano-st777@hotmail.com>
2019-07-29 22:32:11 -0300

    adds test to GET in Form Model

--- spec/requests/api/v1/forms_spec.rb
+++ spec/requests/api/v1/forms_spec.rb
@@ -32,4 +32,45 @@ RSpec.describe "Api::V1::Forms", type: :request do
     end
   end
 
+  describe 'GET /forms/:friendly_id' do
+    before do
+      @user = create(:user)
+    end
+
+    context 'When form exists' do
+      context 'And is enable' do
+        before do
+          @form = create(:form, user: @user, enable: true)
+        end
+
+        it 'returns 200' do
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          expect_status(200)
+        end
+
+        it 'returns Form with right data' do
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          expect(json).to eql(JSON.parse(@form.to_json))
+        end
+      end
+
+      context 'And is unable' do
+        before do
+          @form = create(:form, user: @user, enable: false)
+        end
+
+        it "returns 404" do
+          get "/api/v1/forms/#{FFaker::Lorem.word}", params: {id: @form.friendly_id}, headers: header_with_authentication(@user)
+          expect_status(404)
+        end
+      end
+
+      context "When form dont exists" do
+        it "returns 404" do
+          get "/api/v1/forms/#{FFaker::Lorem.word}", params: {}, headers: header_with_authentication(@user)
+          expect_status(404)
+        end
+      end
+    end
+  end
 end
