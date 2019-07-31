83369ce152b67717da0e4dcc89e82974d600c7c6
Adriano Avelino <adriano-st777@hotmail.com>
2019-07-30 22:14:35 -0300

    adds test to POST in Form Model

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -16,8 +16,17 @@ class Api::V1::FormsController < Api::V1::ApiController
   end
 
   def create
+    @form = Form.new(form_params)
+    if @form.save
+      render json: @form
+    end
   end
 
   def destroy
+  end
+
+  private
+  def form_params
+    params.require(:form).permit(:title, :description, :enable, :primary_color).merge(user: current_api_v1_user)
   end
 end
