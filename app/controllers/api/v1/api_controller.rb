166c6f15320a09cda496e6d0942ff56c1c313c59
Adriano Avelino <adriano-st777@hotmail.com>
2019-07-30 22:14:28 -0300

    adds configuration to ParameterMissing

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -4,6 +4,10 @@ module Api::V1
    rescue_from(ActiveRecord::RecordNotFound) do ||
       render(json: {message: 'Not Found'}, status: :not_found)
    end
+
+   rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+      render(json: {message: "Required parameter missing: #{parameter_missing_exception.param}"}, status: :bad_request)
+   end
  end
 
 end
