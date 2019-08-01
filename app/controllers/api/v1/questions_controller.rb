05d9dd396af750e0de591a4bee3e7631f880f039
Adriano Avelino <adriano-st777@hotmail.com>
2019-07-31 22:22:26 -0300

    adds tests to POST in Question Controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,10 +1,26 @@
-class Api::V1::QuestionsController < ApplicationController
+class Api::V1::QuestionsController < Api::V1::ApiController
+  before_action :authenticate_api_v1_user!
+  before_action :set_form
+
   def create
+    @question = Question.new(question_params.merge(form: @form))
+    if @question.save
+      render json: @question
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def set_form
+    @form = (@question)? @question.form : Form.find(params[:form_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :kind)
   end
 end
