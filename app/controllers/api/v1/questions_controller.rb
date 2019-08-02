9181ce7d63c5d95d78fefcbf0b938f172c8fba57
Adriano Avelino <adriano-st777@hotmail.com>
2019-08-01 21:35:15 -0300

    adds tests to PUt in Question Controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::QuestionsController < Api::V1::ApiController
   before_action :authenticate_api_v1_user!
+  before_action :set_question, only: [:update]
   before_action :set_form
+  before_action :allow_only_owner, only: [:update]
 
   def create
     @question = Question.new(question_params.merge(form: @form))
@@ -10,12 +12,24 @@ class Api::V1::QuestionsController < Api::V1::ApiController
   end
 
   def update
+    if @question.update(question_params)
+      render json: @question
+    end
   end
 
   def destroy
   end
 
   private
+  def allow_only_owner
+    unless current_api_v1_user == @form.user
+      render(json: {}, status: :forbidden) and return
+    end
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
   def set_form
     @form = (@question)? @question.form : Form.find(params[:form_id])
   end
