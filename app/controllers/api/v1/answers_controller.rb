dc2d2b5bee7d82435f2a7e9cce5f04895f4bdfb8
Adriano Avelino <adriano-st777@hotmail.com>
2019-08-04 15:09:13 -0300

    adds tests to action GET in Answer Controller

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,5 +1,10 @@
-class Api::V1::AnswersController < ApplicationController
+class Api::V1::AnswersController < Api::V1::ApiController
+  before_action :authenticate_api_v1_user!
+  before_action :set_form
+
   def index
+    @answers = @form.answers
+    render json: @answers.to_json, include: 'questions_answers'
   end
 
   def show
@@ -9,5 +14,10 @@ class Api::V1::AnswersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def set_form
+    @form = (@answer) ? @answer.form : Form.find(params[:form_id])
   end
 end
--- spec/requests/api/v1/answers_spec.rb
+++ spec/requests/api/v1/answers_spec.rb
@@ -1,10 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Answers", type: :request do
-  describe "GET /Api/v1/answers" do
-    it "works! (now write some real specs)" do
-      get Api_v1_answers_index_path
-      expect(response).to have_http_status(200)
+  before do
+    @user = create(:user)
+  end
+
+  describe 'GET /answers' do
+    context 'With Invalid authentication headers' do
+      it_behaves_like :deny_without_authorization, :get, '/api/v1/answers'
+    end
+
+    context 'With valid authentication headers' do
+      before do
+        @form = create(:form, user: @user)
+        @answer1 = create(:answer, form: @form)
+        @answer2 = create(:answer, form: @form)
+        get api_v1_answers_path , params: {form_id: @form.id}, headers: header_with_authentication(@user)
+      end
+
+      it 'returns 200' do
+        expect_status(200)
+      end
+
+      it "returns Form list with 2 answers" do
+        expect(json.count).to eql(2)
+      end
+
+      it "returned Answers have right datas" do
+        expect(json[0]).to eql(JSON.parse(@answer1.to_json))
+        expect(json[1]).to eql(JSON.parse(@answer2.to_json))
+      end
     end
   end
 end
