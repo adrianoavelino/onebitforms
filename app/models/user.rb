657e786bb3314814d6dcbdd12974c8a680d02718
Adriano Avelino <adriano-st777@hotmail.com>
2019-08-15 22:56:07 -0300

    updates model User for to destroy forms dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+  has_many :forms, dependent: :destroy
 end
